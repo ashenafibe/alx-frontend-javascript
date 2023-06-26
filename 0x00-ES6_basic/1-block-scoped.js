81d3cd72d4f9ae06e91c961ddb451a9c427d4d7d
ashenafibe <111124021+ashenafibe@users.noreply.github.com>
2023-06-26 15:39:15 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line
-    const task = true;
-    // eslint-disable-next-line
-    const task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
   return [task, task2];
